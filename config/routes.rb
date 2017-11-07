1d0f81e257f047327ac84a7be466cd3a75662c7d
menglan wen <wenmenglan33@gmail.com>
2017-11-07 01:45:33 +0000

    recipe creation with test

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   
   get 'pages/home',to: 'pages#home'
-  get '/recipes', to:'recipes#index'
-  get '/recipes/new',to: 'recipes#new', as: 'new_recipe'
-  get '/recipes/:id',to:'recipes#show', as: 'recipe'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :recipes
+  
 end

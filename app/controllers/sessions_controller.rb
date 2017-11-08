3a5780b85bdabb94f62b0c6f851aa13dd107e30e
menglan wen <wenmenglan33@gmail.com>
2017-11-08 05:56:02 +0000

    add restrictions to users not logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,7 @@ def new
   
 end
 
-def create
-  chef = Chef.find_by(params[:session][:email].downcase)
-  if chef && chef.authenticate(params[:session][:password])
-    session[:chef_id]=chef.id
-    flash[:success]="you have login in"
-    redirect_to chef
-  else
-    flash.now[:danger] = "There was something wrong with your login information"
-    render 'new'
-  end
-end
+
 def create
     chef = Chef.find_by(email: params[:session][:email].downcase)
     if chef && chef.authenticate(params[:session][:password])
@@ -25,7 +15,8 @@ def create
     flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
-  end
+end
+  
 def destroy
   session[:chef_id] = nil
   flash[:success] = "You have logged out"
